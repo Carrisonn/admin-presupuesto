84a241c10183e99da779457d9b1d13d0aeb4ca6d
Carlos <carlos97181034535@gmail.com>
2025-03-19 22:27:17 +0100

    added: variables to select html elements and an object to manage user's info

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,10 @@
 export const spanBudget = document.querySelector('#span-budget');
 export const spanRemaining = document.querySelector('#span-remaining');
+export const inputExpense = document.querySelector('#expense');
+export const inputAmount = document.querySelector('#amount');
+export const form = document.querySelector('#form');
+
+export const userInfoObj = {
+  expense: '',
+  amount: ''
+};
