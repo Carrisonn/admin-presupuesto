837bc3cbdfb5bfd43b5d2ca01b6f23220c636658
Carlos <carlos97181034535@gmail.com>
2025-03-19 22:34:44 +0100

    addded: a method to manage the user's expenses

--- src/js/classes/Budget.js
+++ src/js/classes/Budget.js
@@ -1,7 +1,23 @@
+import { UI } from './UI.js';
+import { spanRemaining, form } from '../constants.js';
+
+const ui = new UI();
+
 export class Budget {
   constructor(budget) {
     this.budget = budget;
     this.remaining = budget;
     this.expenses = [];
+  }
+
+  addExpense(userInfoObj) {
+    const { amount } = userInfoObj;
+    this.remaining -= amount;
+    spanRemaining.textContent = this.remaining;
+    this.expenses = [...this.expenses, userInfoObj];
+    ui.showExpenses(this.expenses);
+
+    ui.showNotification('Cantidad restante actualizada', 'success');
+    form.reset();
   }
 }
