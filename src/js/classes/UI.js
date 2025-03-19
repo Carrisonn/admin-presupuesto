21f62b932af612f54c233f21cf4071034f012332
Carlos <carlos97181034535@gmail.com>
2025-03-19 22:36:01 +0100

    added: a method to create a notification

--- src/js/classes/UI.js
+++ src/js/classes/UI.js
@@ -1,11 +1,34 @@
-import { spanBudget, spanRemaining } from '../constants.js';
+import { spanBudget, spanRemaining, form } from '../constants.js';
 
 export class UI {
   constructor() { }
 
-  addBudget(budgetObj) {
+  addBudgetToHTML(budgetObj) {
     const { budget, remaining } = budgetObj;
     spanBudget.textContent = budget;
     spanRemaining.textContent = remaining;
   }
+
+  showNotification(message, type) {
+    const notification = document.createElement('p');
+    notification.classList.add('notification');
+
+    const existNotification = form.querySelector('.notification');
+    existNotification?.remove();
+
+    notification.textContent = message;
+    type === 'error' ? notification.classList.add('error') : notification.classList.add('success');
+    form.appendChild(notification);
+    setTimeout(() => notification.remove(), 4000);
+  }
+
+  //showExpenses(expenses) {
+  //  expenses.forEach(expenseObj => {
+  //    const { expense, amount } = expenseObj;
+  //    console.log(expense, amount);
+  //  });
+  //
+  //
+  //  Object.assign(userInfoObj, { expense: '', amount: '' });
+  //}
 }
