daf54b3d2665a4ee1668874d0eeeca5b81ff0868
Carlos <carlos97181034535@gmail.com>
2025-03-19 22:31:36 +0100

    added: one function to map user's values to an object and other one to validate that values

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,17 +1,34 @@
 import { Budget } from './classes/Budget.js';
 import { UI } from './classes/UI.js';
+import { userInfoObj, spanRemaining } from './constants.js';
 
 const ui = new UI();
 let budget;
 
 export function askBudget() {
-  const userBudget = Number(prompt('¿Cuál es tu presupuesto?'));
-  if (userBudget <= 0 || userBudget === null || isNaN(userBudget)) {
+  const userBudget = prompt('¿Cuál es tu presupuesto?');
+  if (userBudget === '' || userBudget === null || isNaN(userBudget)) {
     alert('Debes ingresar un presupuesto válido');
     return askBudget();
   }
 
   budget = new Budget(userBudget);
-  ui.addBudget(budget);
+  ui.addBudgetToHTML(budget);
+}
 
+export function readUserValues(event) {
+  userInfoObj[event.target.name] = event.target.value.trim().toLowerCase();
+}
+
+export function validateUserInfo(event) {
+  event.preventDefault();
+
+  const { amount } = userInfoObj;
+  const obectValues = Object.values(userInfoObj);
+  const remaining = spanRemaining.textContent;
+
+  if (obectValues.includes('')) return ui.showNotification('Ambos campos son obligatorios', 'error');
+  if (isNaN(amount)) return ui.showNotification('Debes ingresar una cantidad válida', 'error');
+  if (Number(amount) > Number(remaining)) return ui.showNotification('No puedes gastar más que el presupuesto restante', 'error');
+  budget.addExpense(userInfoObj);
 }
