36cb9bc7637195f894761fcb194a5cf5dbba5d3a
Carlos <carlos97181034535@gmail.com>
2025-03-19 22:07:29 +0100

    added: event listeners for inputs and form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
-import { askBudget } from './functions.js';
+import { askBudget, readUserValues, validateUserInfo } from './functions.js';
+import { inputExpense, inputAmount, form } from './constants.js';
 
-document.addEventListener('DOMContentLoaded', () => askBudget());
+window.addEventListener('load', () => form.reset());
+
+document.addEventListener('DOMContentLoaded', () => {
+  askBudget();
+  inputExpense.addEventListener('input', readUserValues);
+  inputAmount.addEventListener('input', readUserValues);
+  form.addEventListener('submit', validateUserInfo);
+});
