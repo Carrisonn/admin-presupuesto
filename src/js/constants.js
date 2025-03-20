9d264b58373e2f7e7c99f366f99ade9cd991a5eb
Carlos <carlos97181034535@gmail.com>
2025-03-20 22:54:00 +0100

    change: an object has been removed and select and html to append content

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,8 +3,4 @@ export const spanRemaining = document.querySelector('#span-remaining');
 export const inputExpense = document.querySelector('#expense');
 export const inputAmount = document.querySelector('#amount');
 export const form = document.querySelector('#form');
-
-export const userInfoObj = {
-  expense: '',
-  amount: ''
-};
+export const divExpense = document.querySelector('.div-expense');
