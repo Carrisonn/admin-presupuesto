68ce7b07ec5fbb27202fd4ede37772cb7cbfe8ea
Carlos <carlos97181034535@gmail.com>
2025-03-20 22:44:00 +0100

    change: removed input's event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
-import { askBudget, readUserValues, validateUserInfo } from './functions.js';
-import { inputExpense, inputAmount, form } from './constants.js';
+import { askBudget, validateUserInfo } from './functions.js';
+import { form } from './constants.js';
 
 window.addEventListener('load', () => form.reset());
 
 document.addEventListener('DOMContentLoaded', () => {
   askBudget();
-  inputExpense.addEventListener('input', readUserValues);
-  inputAmount.addEventListener('input', readUserValues);
   form.addEventListener('submit', validateUserInfo);
 });
