1b6bf5f29ba0fe2fba080774f0a626184c84a75b
Carlos <carlos97181034535@gmail.com>
2025-03-20 22:58:31 +0100

    change: the code has been refactored

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,13 +1,12 @@
 import { Budget } from './classes/Budget.js';
 import { UI } from './classes/UI.js';
-import { userInfoObj, spanRemaining } from './constants.js';
 
 const ui = new UI();
 let budget;
 
 export function askBudget() {
   const userBudget = prompt('¿Cuál es tu presupuesto?');
-  if (userBudget === '' || userBudget === null || isNaN(userBudget)) {
+  if (Number(userBudget) <= 0 || userBudget === null || isNaN(userBudget)) {
     alert('Debes ingresar un presupuesto válido');
     return askBudget();
   }
@@ -16,19 +15,14 @@ export function askBudget() {
   ui.addBudgetToHTML(budget);
 }
 
-export function readUserValues(event) {
-  userInfoObj[event.target.name] = event.target.value.trim().toLowerCase();
-}
-
 export function validateUserInfo(event) {
   event.preventDefault();
 
-  const { amount } = userInfoObj;
-  const obectValues = Object.values(userInfoObj);
-  const remaining = spanRemaining.textContent;
+  const expenseName = document.querySelector('#expense').value.trim().toLowerCase();
+  const expenseAmount = Number(document.querySelector('#amount').value.trim());
 
-  if (obectValues.includes('')) return ui.showNotification('Ambos campos son obligatorios', 'error');
-  if (isNaN(amount)) return ui.showNotification('Debes ingresar una cantidad válida', 'error');
-  if (Number(amount) > Number(remaining)) return ui.showNotification('No puedes gastar más que el presupuesto restante', 'error');
+  if (expenseName === '' || expenseAmount === 0) return ui.showNotification('Ambos campos son obligatorios', 'error');
+  if (expenseAmount < 0) return ui.showNotification('Cantidad no válida', 'error');
+  const userInfoObj = { expenseName, expenseAmount, id: Date.now() };
   budget.addExpense(userInfoObj);
 }
