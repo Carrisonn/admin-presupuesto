7d9841e2466594a78fef9eb3d09ead3ed9ef2be2
Carlos <carlos97181034535@gmail.com>
2025-03-21 18:22:40 +0100

    removed: input selectors

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,7 +1,5 @@
 export const spanBudget = document.querySelector('#span-budget');
 export const spanRemaining = document.querySelector('#span-remaining');
-export const inputExpense = document.querySelector('#expense');
-export const inputAmount = document.querySelector('#amount');
 export const form = document.querySelector('#form');
 export const divExpenseContainer = document.querySelector('#div-expense-conatiner');
 export const divUserExpenseContainer = document.querySelector('.div-user-expense-container');
