c7f3a372f5bb73a9b38dd6c266a20026366e094b
Carlos <carlos97181034535@gmail.com>
2025-03-21 18:20:55 +0100

    added: select more html elements

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,4 +3,6 @@ export const spanRemaining = document.querySelector('#span-remaining');
 export const inputExpense = document.querySelector('#expense');
 export const inputAmount = document.querySelector('#amount');
 export const form = document.querySelector('#form');
-export const divExpense = document.querySelector('.div-expense');
+export const divExpenseContainer = document.querySelector('#div-expense-conatiner');
+export const divUserExpenseContainer = document.querySelector('.div-user-expense-container');
+export const divExpenseRemaining = document.querySelector('.div-expense-remaining');
