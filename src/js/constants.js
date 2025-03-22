ceee58b24fe8989a8508025aa8dda8db29c8ab08
Carlos <carlos97181034535@gmail.com>
2025-03-22 23:44:17 +0100

    added: selected one html element

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,3 +4,4 @@ export const form = document.querySelector('#form');
 export const divExpenseContainer = document.querySelector('#div-expense-conatiner');
 export const divUserExpenseContainer = document.querySelector('.div-user-expense-container');
 export const divExpenseRemaining = document.querySelector('.div-expense-remaining');
+export const paragraphRemaining = document.querySelector('.remaining');
